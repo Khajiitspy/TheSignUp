3b4fd934dcf1ca7a207686fb3e273cf5f0468178
Khajiitspy <150380340+Khajiitspy@users.noreply.github.com>
2025-04-07 11:44:02 -0400

    Added standardized server connection. But javascript is often dumb and does not update changes, so each file redefines the connection to make sure it is defined.

--- js/NavbarActions.js
+++ js/NavbarActions.js
@@ -1,25 +1,25 @@
-﻿const Avatar = document.getElementById('Avatar');
+﻿//const Avatar = document.getElementById('Avatar');
 
-Avatar.onclick = function (event) {
-    location.href = "/Profile.html";
-}
+//Avatar.onclick = function (event) {
+//    location.href = "/Profile.html";
+//}
 
-function RefreshAvatar() {
-    const token = localStorage.getItem("token");
-    axios.get(`${window.API_BASE_URL}/api/Account/profile`, {
-        headers: {
-            'accept': '*/*',
-            'Authorization': `Bearer ${token}`
-        }
-    })
-        .then(resp => {
-            const { data } = resp;
-            const { id, email, firstName, secondName, phone, photo } = data;
-            Avatar.src = `${window.API_BASE_URL}/images/${photo}`;
-        })
-        .catch(err => {
-            console.log("Error", err);
-        });
-}
+//function RefreshAvatar() {
+//    const token = localStorage.getItem("token");
+//    axios.get(`${window.API_BASE_URL}/api/Account/profile`, {
+//        headers: {
+//            'accept': '*/*',
+//            'Authorization': `Bearer ${token}`
+//        }
+//    })
+//        .then(resp => {
+//            const { data } = resp;
+//            const { id, email, firstName, secondName, phone, photo } = data;
+//            Avatar.src = `${window.API_BASE_URL}/images/${photo}`;
+//        })
+//        .catch(err => {
+//            console.log("Error", err);
+//        });
+//}
 
-RefreshAvatar();
+//RefreshAvatar();
